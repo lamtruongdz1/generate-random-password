e4bfe906645958c4a1dc489af24c1fdfbf78c223
lamtruongdz <lamtruongdz2000@gmail.com>
2022-09-28 15:39:21 +0700

    create api

---
+++ app/Http/Controllers/Api/PassWordController.php
@@ -0,0 +1,16 @@
+<?php
+
+namespace App\Http\Controllers\Api;
+
+use App\Http\Controllers\Controller;
+use Illuminate\Http\Request;
+
+class PassWordController extends Controller
+{
+    public function index() {
+        $random = str_shuffle('abcdefghjklmnopqrstuvwxyzABCDEFGHJKLMNOPQRSTUVWXYZ234567890!$%^&!$%^&');
+        $password = substr($random, 0, 15);
+        
+        return response()->json(['password'=>$password], 404);
+    }
+}
